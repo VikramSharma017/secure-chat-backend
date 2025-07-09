9af18965840aa2e6e83fed0b150cc6a6669c2c34
VikramSharma017 <sharma.277@wright.edu>
2025-07-09 14:41:02 -0400

    Added author info to code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# Vikram Sharma
+# Author: Vikram Sharma
+# Project: FastAPI Chat Messaging API
+# Date: 2025-07-09
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
